d51abd4805db1b33b1f45f77d0ec7ea62c8df3f1
pizzaYami <cyj9407@naver.com>
2024-03-27 23:45:30 +0900

    Feat: Login, ProductAll, ProductDetail

---
+++ src/page/ProductAll.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductAll() {
+  return <div>전체상품페이지</div>;
+}
+
+export default ProductAll;
---
+++ src/page/ProductDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductDetail() {
+  return <div>상품 상세 페이지</div>;
+}
+
+export default ProductDetail;
