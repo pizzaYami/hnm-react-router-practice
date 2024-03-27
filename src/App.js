0c9edb10026c951b773e5daa99ac27054afc728a
pizzaYami <cyj9407@naver.com>
2024-03-27 23:45:52 +0900

    Feat: Navbar, fontawsome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
+import Navbar from "./component/Navbar.tsx";
 
 // 1. 전체 상품페이지, 로그인, 상품상세페이지
 // 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
@@ -9,8 +14,18 @@ import "./App.css";
 // 7. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다. 다시 로그인 페이지가 보인다.
 // 8. 로그인을 하면 로그아웃이 보이고 로그아웃을 하며 로그인이 보인다.
 // 9. 상품을 검색할 수 있다.
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+  return (
+    <div>
+      <div>
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+      <div className="nav-section">
+        <img
+          width={100}
+          src="https://upload.wikimedia.org/wikipedia/commons/5/53/H%26M-Logo.svg"
+          alt="hnm-logo"
+        />
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+
+        <div className="menu-input">
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" placeholder="제품 검색" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
